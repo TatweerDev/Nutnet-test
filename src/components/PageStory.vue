add9bbf906fada90bd7a3b796dfaa441f34fda49
Ibrahim <antonutkin777@gmail.com>
2021-07-05 22:38:02 +0300

    Added video popup

--- src/components/PageStory.vue
+++ src/components/PageStory.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-img src="../img/Body-bg.jpg" class="video-image">
+  <q-img src="../img/Body-bg.jpg" class="video-image" @click="showPopup">
     <div class="absolute-full video-overlay">
     </div>
     <div class="video-content">
@@ -7,7 +7,27 @@
       <span class="text-roboto">WATCH OUR STORY</span>
     </div>
   </q-img>
+  <div v-if="isShown" class="video-popup" @click="showPopup">
+    <div class="video-wrapper">
+      <iframe class="video-frame" src="https://www.youtube.com/embed/KvUgaHTNit4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShown: false
+    }
+  },
+  methods: {
+    showPopup() {
+      this.isShown = !this.isShown
+    }
+  }
+}
+</script>
 
 <style lang="sass" scoped>
 .video-image
@@ -45,4 +65,28 @@
   text-align: center
   font-weight: 600
 
+.video-popup
+  position: fixed
+  top: 0
+  left: 0
+  height: 100%
+  width: 100%
+  background: rgba(0,0,0,.5)
+  backdrop-filter: blur(50px)
+  z-index: 11
+  cursor: pointer
+
+.video-wrapper
+  position: absolute
+  left: 50%
+  top: 50%
+  transform: translate(-50%, -50%)
+  width: 900px
+  height: 700px
+
+.video-frame
+  width: 100%
+  height: 100%
+
+  
 </style>
